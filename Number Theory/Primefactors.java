40d1d8672a0974b224f83136c46ea1c8742617f8
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-09-06 19:15:10 +0530

    Update Primefactors.java

--- Number Theory/Primefactors.java
+++ Number Theory/Primefactors.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) {//TC --> O(sqrt(n))
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int x = n;
