aa2886850c675a82938f298573cc2b795ff1c0e3
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-09-25 18:58:53 +0530

    Create TwoSortedArrays.java

---
+++ Arrays/Counting inversion/TwoSortedArrays.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int a[] = {2,4,6,8};
+		int b[] = {1,5,7,8};
+		int res = 0;
+		int j=0;
+		for(int i=0;i<b.length;i++)
+		{
+		    while(i<b.length && j<a.length && b[i] >= a[j])
+		    {
+		        j++;
+		    }
+		    res+=(j);
+		}
+		System.out.println(res);
+	}
+}
+
+// TC
+
+// O(m+n)
