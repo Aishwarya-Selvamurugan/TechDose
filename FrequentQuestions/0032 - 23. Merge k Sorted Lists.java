3ac1b3542e325ade4b3666e1cbdfeb23c0caaad6
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-10-11 15:06:04 +0530

    Create 0032 - 23. Merge k Sorted Lists.java

---
+++ FrequentQuestions/0032 - 23. Merge k Sorted Lists.java
@@ -0,0 +1,49 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int l = lists.length;
+        return MergeSort(lists,0,l-1);
+    }
+    public static ListNode MergeSort(ListNode[] lists,int low,int high)
+    {
+        if(low < high)
+        {
+            int mid = low + ((high-low)/2);
+            ListNode l1 = MergeSort(lists,low,mid);
+            ListNode l2 = MergeSort(lists,mid+1,high);
+            return Merge(l1,l2);
+        }
+        return (low>high)?null:lists[low];
+    }
+    public static ListNode Merge(ListNode l1,ListNode l2)
+    {
+        ListNode l = new ListNode();
+        ListNode root = l;
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val > l2.val)
+            {
+                l.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            else
+            {
+                l.next = new ListNode(l1.val);
+                l1 = l1.next;
+            }
+            l = l.next;
+        }
+        while(l1!=null)
+        {
+            l.next = new ListNode(l1.val);
+            l1 = l1.next;
+            l = l.next;
+        }
+        while(l2!=null)
+        {
+            l.next = new ListNode(l2.val);
+            l2 = l2.next;
+            l = l.next;
+        }
+        return root.next;
+    }
+}
