384e354979c68298d94508325d22affc2417ea73
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-10-18 19:43:55 +0530

    Create 141. Linked List Cycle.java

---
+++ Linked List/141. Linked List Cycle.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> h = new HashSet<>();
+        while(head!=null)
+        {
+            if(h.contains(head))
+            {
+                return true;
+            }
+            h.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
