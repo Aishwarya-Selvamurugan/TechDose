e00c921236e0a48587e2d0b530dff8a8e5f26a36
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-10-20 09:59:41 +0530

    Update 141. Linked List Cycle.java

--- Linked List/141. Linked List Cycle.java
+++ Linked List/141. Linked List Cycle.java
@@ -13,3 +13,32 @@ public class Solution {
         return false;
     }
 }
+
+/*
+    Time complexity: O(n). 
+    Only one traversal of the loop is needed.
+    Auxiliary Space: O(n). 
+    n is the space required to store the value in hashmap.
+*/
+
+/*Floyd’s Cycle-Finding Algorithm (This is the fastest method)
+TC : Time complexity: O(n). 
+Only one traversal of the loop is needed.
+Auxiliary Space:O(1). 
+*/
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head,fast = head;
+        while(slow !=null && fast != null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
