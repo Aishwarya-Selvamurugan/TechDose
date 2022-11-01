08c4e34850d193a2c2f8f2af112f7cd4d9d96576
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-11-01 11:04:23 +0530

    Create All possible subset.java

---
+++ Recursion/All possible subset.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] s = str.toCharArray();
+        all_possible_subset(s,0," ");
+
+    }
+    public static void all_possible_subset(char[] s,int l,String curr)
+    {
+        if(l == s.length)
+        {
+            System.out.println(curr);
+            return;
+        }
+        all_possible_subset(s,l+1,curr);
+        all_possible_subset(s,l+1,curr+s[l]);
+    }
+}
