020506306b82b9b0a94fd9be8941c0257da64b72
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-11-03 16:06:54 +0530

    Create 22. Generate Parentheses.java

---
+++ Recursion/Backtracking Assignment/22. Generate Parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        Valid(n,0,0,"");
+        return res;
+    }
+    public void Valid(int n,int o,int e,String s)
+    {
+        if(s.length() == n*2)
+        {
+            res.add(s);
+            return;
+        }
+        if(o < n)
+        {
+            Valid(n,o+1,e,s+"(");
+        }
+        if(e < o)
+        {
+            Valid(n,o,e+1,s+")");
+        }
+    }
+}
