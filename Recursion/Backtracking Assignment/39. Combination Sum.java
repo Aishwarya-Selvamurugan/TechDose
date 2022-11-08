34c08ed33312d86baac044d5db25c2122284ee12
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-11-08 10:22:48 +0530

    Update 39. Combination Sum.java

--- Recursion/Backtracking Assignment/39. Combination Sum.java
+++ Recursion/Backtracking Assignment/39. Combination Sum.java
@@ -27,3 +27,36 @@ class Solution {
         l.remove(l.size()-1);
     }
 }
+
+
+/*<=======================================================================================================>*/
+    
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Sum(candidates,target,0,0,new ArrayList<>());
+        return res;
+    }
+    public void Sum(int[] nums,int target,int sum,int index,List<Integer> l)
+    {
+        if(index == nums.length)
+        {
+            return;
+        }
+        if(sum == target)
+        {
+            // System.out.println(l);
+            res.add(new ArrayList<>(l));
+            return;
+        }
+        for(int i=index;i<nums.length;i++)
+        {
+            if(target >= sum)
+            {
+                l.add(nums[i]);
+                Sum(nums,target,sum+nums[i],i,l);
+                l.remove(l.size()-1);
+            }
+        }
+    }
+}    
