507003c084aaffbfe40c7241ad50be3b2804222a
Aishwarya-Selvamurugan <85281914+Aishwarya-Selvamurugan@users.noreply.github.com>
2022-12-28 12:51:06 +0530

    Update 207. Course Schedule.java

--- Graph/Assignment/207. Course Schedule.java
+++ Graph/Assignment/207. Course Schedule.java
@@ -42,3 +42,56 @@ class Solution {
         return count == in.length;
     }
 }
+
+=========================================================================================
+ 
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] g = new ArrayList[numCourses];
+        for(int i=0;i<numCourses;i++)
+        {
+            g[i] = new ArrayList<>();
+        }
+        int in[] = new int[numCourses];
+        for(int i=0;i<prerequisites.length;i++)
+        {
+            g[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+        return formscycle(g);
+    }
+    public boolean formscycle(List<Integer>[] g)
+    {
+        int[] v = new int[g.length];
+        for(int i=0;i<g.length;i++)
+        {
+            if(v[i] == 0)
+            {
+                if(dfs(i,v,g))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean dfs(int n,int[] v,List<Integer>[] g)
+    {
+        if(v[n] == 1)
+        {
+            return true;
+        }
+        if(v[n] == 2){
+            return false;
+        }
+        v[n] = 1;
+        for(int i=0;i<g[n].size();i++)
+        {
+            if(dfs(g[n].get(i),v,g))
+            {
+                return true;
+            }
+        }
+        v[n] = 2;
+        return false;
+    }
+}    
